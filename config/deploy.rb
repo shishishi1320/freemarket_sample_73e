61bafe4b8ad1798ec87921efc354f014d8f28a2f
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-04-22 21:04:32 +0900

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.13.0"
 
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'freemarket_sample_73e'
-set :repo_url,  'taiyakin/freemarket_sample_73e'
+set :repo_url,  'git@github.com:taiyakin/freemarket_sample_73e.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 

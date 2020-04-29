5b310777c6e12385a7c982cb6055a7b1709daf46
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-29 12:31:19 +0900

    Add img-upload

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
   belongs_to :item
-  mount_uploader :src, ImageUploader
+  mount_uploader :url, ImageUploader
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   # belongs_to :category_id
   # belongs_to :category_id
   has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 end

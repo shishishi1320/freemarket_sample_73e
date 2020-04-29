24eeb8fec5ff1765c996b711459932e1f1955a18
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-29 11:32:19 +0900

    img upload base

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,74 +1,64 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /items
-  # GET /items.json
-  def index
-    @items = Item.all
-  end
+  # # GET /items
+  # # GET /items.json
+  # def index
+  #   @items = Item.all
+  # end
 
-  # GET /items/1
-  # GET /items/1.json
-  def show
-  end
+  # # GET /items/1
+  # # GET /items/1.json
+  # def show
+  # end
 
-  # GET /items/new
   def new
     @item = Item.new
+    @item.images.new
   end
 
-  # GET /items/1/edit
-  def edit
-  end
+  # # GET /items/1/edit
+  # def edit
+  # end
 
-  # POST /items
-  # POST /items.json
   def create
     @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
-  def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @item.update(item_params)
+  #       format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @item }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @item.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
-  def destroy
-    @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /items/1
+  # # DELETE /items/1.json
+  # def destroy
+  #   @item.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_item
+    #   @item = Item.find(params[:id])
+    # end
 
-    # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price, :text, :status, :size, :brand_id_id, :condition, :shipping_cost, :delivery_method, :delivery_area, :delivery_date, :category_id_id, :category_id_id, :seller_id, :buyer_id)
+      params.require(:item).permit(:name, :price, :text, :status, :size, :brand_id, :condition, :shipping_cost, :delivery_method, :delivery_area, :delivery_date, :category_id, :seller_id, :buyer_id, images_attributes: [:url])
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ApplicationRecord
-  belongs_to :item_id
+  belongs_to :item
+  mount_uploader :url, ImageUploader
 end

064fb79ac2e313cc69bb9439e4e94f681b0c54aa
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-29 11:32:19 +0900

    img upload preparation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ApplicationRecord
-  belongs_to :item_id
+  mount_uploader :src, ImageUploader
+  belongs_to :item
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :brand_id
-  belongs_to :category_id
-  belongs_to :category_id
+  # belongs_to :brand_id
+  # belongs_to :category_id
+  # belongs_to :category_id
+  has_many :images
 end

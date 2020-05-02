81395991191b7c6804112b80d36e10b01e7e6de5
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-02 19:15:33 +0900

    Fix controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'Event was successfully created.'
     else
       render :new
     end

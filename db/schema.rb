e54f4a3cda41ce39c158fcc0fff92afc85874cac
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-05-04 14:20:47 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2020_05_02_020715) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "credit_cards", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_id"
+    t.integer "customer_id"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
+
   create_table "images", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "url", null: false
     t.integer "item_id", null: false
@@ -102,6 +111,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_020715) do
   add_foreign_key "addresses", "users"
   add_foreign_key "brands_categories", "brands"
   add_foreign_key "brands_categories", "categories"
+  add_foreign_key "credit_cards", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"

b301d8d823b5ae8a7b3e96c863efb55d27f4f329
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-05-04 17:03:16 +0900

    a

--- db/migrate/20200427113843_create_credit_cards.rb
+++ db/migrate/20200427113843_create_credit_cards.rb
@@ -4,7 +4,6 @@ class CreateCreditCards < ActiveRecord::Migration[5.0]
       t.integer :card_id
       t.integer :customer_id
       t.references :user, null: false,foreign_key: true
-
       t.timestamps
     end
   end

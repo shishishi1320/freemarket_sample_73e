1713ee43111aa9ba4db3543be984d1904b48b657
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-06 11:15:55 +0900

    edit js

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file main__content__img-up__file__l__field__file" type="file"
@@ -9,7 +8,6 @@ $(document).on('turbolinks:load', ()=> {
                   </div>`;
     return html;
   }
-  // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
     const html = `
     <img data-index="${index}" src="${url}" width="118px" height="118px">
@@ -17,9 +15,8 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
 
-  // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 既に使われているindexを除外
+
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
 
@@ -27,19 +24,15 @@ $(document).on('turbolinks:load', ()=> {
 
   $('#image-box').on('change', '.js-file', function(e) {
       const targetIndex = $(this).parent().data('index');
-      // ファイルのブラウザ上でのURLを取得する
       const file = e.target.files[0];
       const blobUrl = window.URL.createObjectURL(file);
-      // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
       if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
         img.setAttribute('url', blobUrl);
-      } else {  // 新規画像追加の処理
+      } else {
         $('#previews').append(buildImg(targetIndex, blobUrl));
         if ( fileIndex[0] < 10 ){
-          // fileIndexの先頭の数字を使ってinputを作る
           $('#image-box').append(buildFileField(fileIndex[0]));
           fileIndex.shift();
-          // 末尾の数に1足した数を追加する
           fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
         }
       }
@@ -47,15 +40,12 @@ $(document).on('turbolinks:load', ()=> {
 
   $('#image-box').on('click', '.js-remove', function() {
     const targetIndex = $(this).parent().data('index');
-    // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
     if (hiddenCheck) hiddenCheck.prop('checked', true);
 
     $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
 
-    // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });

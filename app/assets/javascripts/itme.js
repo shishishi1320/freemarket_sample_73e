5ddaddeba3c5db9380cc71cfeb3947fc90b1d8ed
shishishi1320 <shishishi.1320@gmail.com>
2020-05-06 11:41:25 +0900

    画像表示修正

--- app/assets/javascripts/itme.js
+++ app/assets/javascripts/itme.js
@@ -1,9 +1,9 @@
 $(function () {
-  $(".image__sub__thumb").first().addClass("active"); // 1枚目の小画像をアクティブに変更
-  $(".image__sub__thumb__photo").click(function () { // 小画像がクリックされたらイベント発火
+  $(".itembox__body__image__sub__thumb").first().addClass("active"); // 1枚目の小画像をアクティブに変更
+  $(".itembox__body__image__sub__thumb__photo").click(function () { // 小画像がクリックされたらイベント発火
     image_url = $(this).attr("src"); // クリックされた画像のPATHを取得
-    $(".image__main__photo").attr("src", image_url).hide().fadeIn(); // メイン画像をクリックされた画像に変更
-    $(".image__sub__thumb.active").removeClass("active"); // 1枚目の小画像のアクティブを無効化
+    $(".itembox__body__image__main__photo").attr("src", image_url).hide().fadeIn(); // メイン画像をクリックされた画像に変更
+    $(".itembox__body__image__sub__thumb.active").removeClass("active"); // 1枚目の小画像のアクティブを無効化
     $(this).parent().addClass("active"); // クリックされた小画像をアクティブに変更
   });
 });

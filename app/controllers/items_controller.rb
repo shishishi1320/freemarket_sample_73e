2c68d7e789e612158f867abdc164adb523533b78
shishishi1320 <shishishi.1320@gmail.com>
2020-05-06 18:18:29 +0900

    商品詳細ページの画像修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,6 +62,7 @@ class ItemsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
+      
     end
   end
 

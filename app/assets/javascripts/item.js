2e01a2b141f78609c08052d72fb2538441901d10
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-07 12:21:23 +0900

    Fix item.js

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -54,6 +54,7 @@ $(document).on('turbolinks:load', ()=> {
     }
     else {$("#delivery_method-parent").remove();}
 
+  });
   // 画像プレビュー
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="js-file_group">
@@ -105,5 +106,4 @@ $(document).on('turbolinks:load', ()=> {
 
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
-});
 });
